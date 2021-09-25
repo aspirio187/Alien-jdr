001e5379a9c99b60fb5521a29c74b0e6b7c7377b
Soultan Hatsijev <soultan_98@hotmail.com>
2021-09-26 01:12:11 +0200

    feat: Dtos for User connection and login, repository and profile for mapping from dto to entity

---
+++ Alien-JDR/Alien.BLL/Profiles/UserProfile.cs
@@ -0,0 +1,20 @@
+﻿using Alien.BLL.Dtos;
+using Alien.DAL.Entities;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alien.BLL.Profiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<UserSignUpDto, UserEntity>();
+            CreateMap<UserSignInDto, UserEntity>();
+        }
+    }
+}
---
+++ Alien-JDR/Alien.BLL/Repositories/UserRepository.cs
@@ -0,0 +1,40 @@
+﻿using Alien.BLL.Dtos;
+using Alien.DAL.Entities;
+using Alien.DAL.IServices;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alien.BLL.Repositories
+{
+    public class UserRepository
+    {
+        private readonly IUserService _userService;
+        private readonly IMapper _mapper;
+
+        public UserRepository(IUserService userService, IMapper mapper)
+        {
+            _userService = userService ??
+                throw new ArgumentNullException(nameof(userService));
+            _mapper = mapper ??
+                throw new ArgumentNullException(nameof(mapper));
+        }
+
+        public bool SignUp(UserSignUpDto user)
+        {
+            if (user is null) throw new ArgumentNullException(nameof(user));
+            _userService.Create(_mapper.Map<UserEntity>(user));
+            return _userService.SaveChanges();
+        }
+
+        public string SignIn(UserSignInDto user)
+        {
+            if (user is null) throw new ArgumentNullException(nameof(user));
+
+            return string.Empty;
+        }
+    }
+}
