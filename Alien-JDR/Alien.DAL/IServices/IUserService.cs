3fb816decce953c9aa842ac152fd9019f7d1aaa3
Soultan Hatsijev <soultan_98@hotmail.com>
2021-09-25 21:23:35 +0200

    feat: Added the first Service Interface for UserEntity

---
+++ Alien-JDR/Alien.DAL/IServices/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using Alien.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alien.DAL.IServices
+{
+    public interface IUserService
+    {
+        Task<bool> SignInAsync(string email, string password);
+        Task<bool> SignUpAsync();
+        Task<UserEntity> GetUserAsync();
+        Task<IEnumerable<UserEntity>> GetUsersAsync();
+    }
+}
