38ac6ff564b5f389835ea38db8921ba480a50d7f
Soultan Hatsijev <soultan_98@hotmail.com>
2021-09-25 11:35:10 +0200

    feat: Created User Entity with its configuration

---
+++ Alien-JDR/Alien.DAL/Configurations/UserConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Alien.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alien.DAL.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<UserEntity>
+    {
+        public void Configure(EntityTypeBuilder<UserEntity> builder)
+        {
+            builder.Property(x => x.Username)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.HasIndex(x => x.Username)
+                .IsUnique();
+        }
+    }
+}
