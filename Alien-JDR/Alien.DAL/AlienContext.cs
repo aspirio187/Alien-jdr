5960365a615bc7d7a93cb1c8b40e8961bd876037
Soultan Hatsijev <soultan_98@hotmail.com>
2021-09-25 11:43:22 +0200

    feat: Added DbSet<User> and reference from UI to DAL

--- Alien-JDR/Alien.DAL/AlienContext.cs
+++ Alien-JDR/Alien.DAL/AlienContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Alien.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,8 @@ namespace Alien.DAL
 {
     public class AlienContext : DbContext
     {
+        public DbSet<UserEntity> Users { get; set; }
+
         public AlienContext()
         {
 
