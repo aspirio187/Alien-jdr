a1734408cc92b073775a19b618471de5f26dd46e
Soultan Hatsijev <soultan_98@hotmail.com>
2021-09-25 23:33:33 +0200

    feat: add comments for the functions

--- Alien-JDR/Alien.DAL/Services/BaseService.cs
+++ Alien-JDR/Alien.DAL/Services/BaseService.cs
@@ -21,36 +21,60 @@ namespace Alien.DAL.Services
 
 
         /// <summary>
-        /// Create a generic Entity based on the type declared in the class declaration
+        /// Create an entity in the database
         /// </summary>
         /// <param name="entity"></param>
+        /// <exception cref="ArgumentNullException"></exception>
         public virtual void Create(T entity)
         {
             if (entity is null) throw new ArgumentNullException(nameof(entity));
             _context.Entry(entity).State = EntityState.Added;
         }
 
+        /// <summary>
+        /// Delete the entity from the database
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <exception cref="ArgumentNullException"></exception>
         public virtual void Delete(T entity)
         {
             if (entity is null) throw new ArgumentNullException(nameof(entity));
             _context.Entry(entity).State = EntityState.Deleted;
         }
 
+        /// <summary>
+        /// Return the complete list of entities asynchronously
+        /// </summary>
+        /// <returns>IEnumerable <typeparamref name="T"/> </returns>
         public virtual async ValueTask<IEnumerable<T>> GetEntitiesAsync()
         {
             return await _context.Set<T>().ToListAsync();
         }
 
+        /// <summary>
+        /// Get an entity from the database by its ID asynchronously
+        /// </summary>
+        /// <param name="id">ID of the entity</param>
+        /// <returns><typeparamref name="T"/></returns>
         public virtual async ValueTask<T> GetEntityAsync(int id)
         {
             return await _context.Set<T>().FindAsync(id);
         }
 
+        /// <summary>
+        /// Save changes from the context to the database
+        /// </summary>
+        /// <returns>True if one or more changes were sent to the database. false Otherwise</returns>
         public bool SaveChanges()
         {
             return _context.SaveChanges() > 0;
         }
 
+        /// <summary>
+        /// Update the entity from the database
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <exception cref="ArgumentNullException"></exception>
         public virtual void Update(T entity)
         {
             if (entity is null) throw new ArgumentNullException(nameof(entity));
