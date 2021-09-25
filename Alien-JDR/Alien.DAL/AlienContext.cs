cfe22dfb067f43bf833a605affa316407fb15be2
Soultan Hatsijev <soultan_98@hotmail.com>
2021-09-25 11:22:21 +0200

    feat: Added Nugget packages for each project

---
+++ Alien-JDR/Alien.DAL/AlienContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alien.DAL
+{
+    public class AlienContext : DbContext
+    {
+        public AlienContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
