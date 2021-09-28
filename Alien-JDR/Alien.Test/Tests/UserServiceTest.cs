1d8c3a73e88b013a7d905783ac25fe94b95cf4ba
Soultan Hatsijev <soultan_98@hotmail.com>
2021-09-28 13:27:29 +0200

    feat: Added NUnit test project

---
+++ Alien-JDR/Alien.Test/Tests/UserServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alien.Test.Tests
+{
+    public class UserServiceTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+    }
+}
