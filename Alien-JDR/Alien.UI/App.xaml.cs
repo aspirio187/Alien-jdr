53dd1c8cd74995112ff93105541cb27537c35535
Soultan Hatsijev <soultan_98@hotmail.com>
2021-10-08 19:36:18 +0200

    feat: Added first navigation test with prism and ShellView with their viewmodels

--- Alien-JDR/Alien.UI/App.xaml.cs
+++ Alien-JDR/Alien.UI/App.xaml.cs
@@ -1,17 +1,30 @@
-﻿using System;
+﻿using Prism.Unity;
+using Prism.Ioc;
+using Prism.Modularity;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Alien.UI.Views;
 
 namespace Alien.UI
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<ShellView>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<CharacterHomeView>();
+        }
     }
 }
