a3a98d78bf73479f35fc4ea5ee47b2a2a3fb8963
David Arustamyan <david.arustamyan.96@gmail.com>
2021-11-01 20:57:53 +0100

    Fixed few errors in views

--- Alien-JDR/Alien.UI/ViewModels/ShellViewModel.cs
+++ Alien-JDR/Alien.UI/ViewModels/ShellViewModel.cs
@@ -10,63 +10,63 @@ using Alien.UI.Views;
 
 namespace Alien.UI.ViewModels
 {
-    //public class ShellViewModel : BindableBase 
-    //{
-    //    private readonly RegionManager _regionManager;
-    //    private readonly RegionNavigationService _regionNavigationService;
-
-    //    public ShellViewModel(RegionManager regionManager, RegionNavigationService regionNavigationService)
-    //    {
-    //        _regionManager = regionManager ?? throw new ArgumentNullException(nameof(regionManager));
-    //        _regionNavigationService = regionNavigationService ?? throw new ArgumentNullException(nameof(regionNavigationService));
-    //    }
-
-    //    public void Load()
-    //    {
-    //        _regionNavigationService.Region = _regionManager.Regions["MainRegion"];
-    //        _regionNavigationService.RequestNavigate(new Uri("CharacterHomeView", UriKind.Relative), null);
-    //    }
-    //}
-
-    public class ShellViewModel : ObservableObject
+    public class ShellViewModel : BindableBase
     {
-        public RelayCommand CharacterHomeViewCommand { get; set; }
+        private readonly RegionManager _regionManager;
+        private readonly RegionNavigationService _regionNavigationService;
 
-        public RelayCommand GamesViewCommand { get; set; }
-
-        public CharacterHomeViewModel CharacterVM { get; set; }
-
-        public GameViewModel GamesVM { get; set; }
-
-        private object _currentView;
-
-        public object CurrentView
+        public ShellViewModel(RegionManager regionManager, RegionNavigationService regionNavigationService)
         {
-            get { return _currentView; }
-            set
-            {
-                _currentView = value;
-                OnPropertyChanged();
-            }
+            _regionManager = regionManager ?? throw new ArgumentNullException(nameof(regionManager));
+            _regionNavigationService = regionNavigationService ?? throw new ArgumentNullException(nameof(regionNavigationService));
         }
 
-
-        public ShellViewModel()
+        public void Load()
         {
-            CharacterVM = new CharacterHomeView();
-            GamesVM = new GameViewModel();
-
-            CurrentView = CharacterVM;
-
-            CharacterHomeViewCommand = new RelayCommand(o =>
-            {
-                CurrentView = CharacterVM;
-            });
-
-            GamesViewCommand = new RelayCommand(o =>
-            {
-                CurrentView = GamesVM;
-            });
+            _regionNavigationService.Region = _regionManager.Regions["MainRegion"];
+            _regionNavigationService.RequestNavigate(new Uri("CharacterHomeView", UriKind.Relative), null);
         }
     }
+
+    //public class ShellViewModel : ObservableObject
+    //{
+    //    public RelayCommand CharacterHomeViewCommand { get; set; }
+
+    //    public RelayCommand GamesViewCommand { get; set; }
+
+    //    public CharacterHomeViewModel CharacterVM { get; set; }
+
+    //    public GameViewModel GamesVM { get; set; }
+
+    //    private object _currentView;
+
+    //    public object CurrentView
+    //    {
+    //        get { return _currentView; }
+    //        set
+    //        {
+    //            _currentView = value;
+    //            OnPropertyChanged();
+    //        }
+    //    }
+
+
+    //    public ShellViewModel()
+    //    {
+    //        CharacterVM = new CharacterHomeView();
+    //        GamesVM = new GameViewModel();
+
+    //        CurrentView = CharacterVM;
+
+    //        CharacterHomeViewCommand = new RelayCommand(o =>
+    //        {
+    //            CurrentView = CharacterVM;
+    //        });
+
+    //        GamesViewCommand = new RelayCommand(o =>
+    //        {
+    //            CurrentView = GamesVM;
+    //        });
+    //    }
+    //}
 }
