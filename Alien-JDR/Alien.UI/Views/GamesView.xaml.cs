ddb4d9ef8bd55346a2766d4bd0dc27bde3312a25
David Arustamyan <david.arustamyan.96@gmail.com>
2021-11-06 16:14:56 +0100

    solved the fonts issue, added dataGrid in gameView, binded with gamesClasse

--- Alien-JDR/Alien.UI/Views/GamesView.xaml.cs
+++ Alien-JDR/Alien.UI/Views/GamesView.xaml.cs
@@ -23,6 +23,43 @@ namespace Alien.UI.Views
         public GamesView()
         {
             InitializeComponent();
+            games game1 = new games();
+
+            game1.gameID = "1";
+            game1.playerID = "David";
+            game1.gameMode = "Compagne";
+            game1.creator = "Soultan";
+
+            GamesDataGrid.Items.Add(game1);
+
+            games game2 = new games();
+
+            game2.gameID = "2";
+            game2.playerID = "Soultan";
+            game2.gameMode = "Custom";
+            game2.creator = "David";
+
+            GamesDataGrid.Items.Add(game2);
+        }
+
+        public class games
+        {
+            public string gameID { get; set; }
+            public string playerID { get; set; }
+            public string gameMode { get; set; }
+            public string creator { get; set; }
+        }
+
+        private void CreateAGame_Click(object sender, RoutedEventArgs e)
+        {
+            games tempGames = new games();
+
+            tempGames.gameID = gameID.Text;
+            tempGames.playerID = playerID.Text;
+            tempGames.gameMode = gameMode.Text;
+            tempGames.creator = creator.Text;
+
+            GamesDataGrid.Items.Add(tempGames);
         }
     }
 }
