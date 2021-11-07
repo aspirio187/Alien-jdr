ccab6cdd16cf839a34db938f6bde4c30d7ff192c
Soultan Hatsijev <soultan_98@hotmail.com>
2021-11-07 18:22:20 +0100

    feat: Configured RegistrationViewModel

--- Alien-JDR/Alien.UI/ViewModels/RegistrationViewModel.cs
+++ Alien-JDR/Alien.UI/ViewModels/RegistrationViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Alien.UI.States;
+using Prism.Mvvm;
+using Prism.Services.Dialogs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,27 @@ using System.Threading.Tasks;
 
 namespace Alien.UI.ViewModels
 {
-    class RegistrationViewModel
+    public class RegistrationViewModel : BindableBase, IDialogAware
     {
+        private readonly IAuthenticator _authenticator;
+
+        public string Title => "Inscription";
+
+        public event Action<IDialogResult> RequestClose;
+
+        public bool CanCloseDialog()
+        {
+            return true;
+        }
+
+        public void OnDialogClosed()
+        {
+            // TODO : Si l'inscription s'est bien passé, retourner en arrière, sinon fermer le programme
+        }
+
+        public void OnDialogOpened(IDialogParameters parameters)
+        {
+            // TODO : Rien
+        }
     }
 }
