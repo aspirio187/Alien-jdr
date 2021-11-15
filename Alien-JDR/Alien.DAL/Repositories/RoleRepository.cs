3f0a92038d898d3fde81dcf40e0b30611a0bed0b
Diego Boels <diego.boels@hotmail.com>
2021-11-15 18:23:07 +0100

    10DAL Add role conf / Irepository / Repository

---
+++ Alien-JDR/Alien.DAL/Repositories/RoleRepository.cs
@@ -0,0 +1,27 @@
+﻿using Alien.DAL.Entities;
+using Alien.DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alien.DAL.Repositories
+{
+    public class RoleRepository : RepositoryBase<RoleEntity, Guid>, IRoleRepository
+    {
+        public RoleRepository(AlienContext context)
+            : base(context)
+        {
+
+        }
+
+        public async Task<IEnumerable<RoleEntity>> GetUserRoleAsync(Guid userId)
+        {
+            if (userId == Guid.Empty) throw new ArgumentException($"User ID \"{userId}\" is empty!");
+            if (!await _context.Users.AnyAsync(u => u.Id == userId)) throw new Exception($"The user with ID \"{userId}\" doesn't exist!");
+            return (await _context.Users.FindAsync(userId)).Roles.ToList();
+        }
+    }
+}
