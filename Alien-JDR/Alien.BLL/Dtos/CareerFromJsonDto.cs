b82d8025d97b35ae8c05c50a31252b895dc3ce27
Soultan Hatsijev <soultan_98@hotmail.com>
2021-11-17 21:44:04 +0100

    feat: Added dto Career loaded from json file. Added a object in the json Carreers

---
+++ Alien-JDR/Alien.BLL/Dtos/CareerFromJsonDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alien.BLL.Dtos
+{
+    public class CareerFromJsonDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string KeyAttribute { get; set; }
+        public IEnumerable<string> Competences { get; set; }
+        public IEnumerable<string> Talents { get; set; }
+        public IEnumerable<string> TypicalNames { get; set; }
+    }
+}
