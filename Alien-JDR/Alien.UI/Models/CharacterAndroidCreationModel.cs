7f0f0a023426e65d36c83bc912184101f29c45a6
Soultan Hatsijev <soultan_98@hotmail.com>
2021-12-01 17:25:26 +0100

    feat: Created the model for AndroidCreation view. Added method in VM and refactored the view to fit correctly

---
+++ Alien-JDR/Alien.UI/Models/CharacterAndroidCreationModel.cs
@@ -0,0 +1,36 @@
+﻿using Alien.UI.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alien.UI.Models
+{
+    public class CharacterAndroidCreationModel : ModelBase
+    {
+        private Attributes _attribute;
+
+        public Attributes Attribute
+        {
+            get { return _attribute; }
+            set
+            {
+                _attribute = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private bool _isSelected;
+
+        public bool IsSelected
+        {
+            get { return _isSelected; }
+            set
+            {
+                _isSelected = value;
+                NotifyPropertyChanged();
+            }
+        }
+    }
+}
--- Alien-JDR/Alien.UI/ViewModels/CharacterCreation/CharacterAndroidCreationViewModel.cs
+++ Alien-JDR/Alien.UI/ViewModels/CharacterCreation/CharacterAndroidCreationViewModel.cs
@@ -1,12 +1,80 @@
-﻿using System;
+﻿using Alien.BLL.Dtos;
+using Alien.UI.Helpers;
+using Alien.UI.Models;
+using Alien.UI.States;
+using Prism.Commands;
+using Prism.Regions;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Alien.UI.ViewModels
 {
-    class CharacterAndroidCreationViewModel
+    public class CharacterAndroidCreationViewModel : ViewModelBase, IJournalAware
     {
+        public CharacterCreationDto CharacterCreation { get; set; }
+
+        public ObservableCollection<CharacterAndroidCreationModel> CharacterAndroids { get; set; } = new();
+
+        private DelegateCommand _navigateBackCommand;
+        private DelegateCommand _navigateNextPageCommand;
+
+        public DelegateCommand NavigateBackCommand => _navigateBackCommand ??= new(NavigateBack);
+        public DelegateCommand NavigateNextPageCommand => _navigateNextPageCommand ??= new(NavigateNextPage);
+
+        public CharacterAndroidCreationViewModel(IRegionNavigationService regionNavigationService, IAuthenticator authenticator)
+            : base(regionNavigationService, authenticator)
+        {
+
+        }
+
+        public void NavigateBack()
+        {
+            if (_regionNavigationService.Journal.CanGoBack)
+            {
+                _regionNavigationService.Journal.GoBack();
+            }
+        }
+
+        public bool CanSelectAttribute(Attributes? attribute)
+        {
+            if (attribute is null) return false;
+            return false;
+        }
+
+        public void SelectAttribute(Attributes? attribute)
+        {
+
+        }
+
+        public bool CanNavigateNextPage()
+        {
+            return CharacterAndroids.Count(c => c.IsSelected) == 2;
+        }
+
+        public void NavigateNextPage()
+        {
+            Dictionary<string, object> parameters = new Dictionary<string, object>()
+            {
+                { Global.CHARACTER_CREATION, CharacterCreation }
+            };
+
+            Navigate(ViewsEnum.CharacterCreationSummaryView, parameters);
+        }
+
+        public override void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            base.OnNavigatedTo(navigationContext);
+
+            CharacterCreation = navigationContext.Parameters.GetValue<CharacterCreationDto>(Global.CHARACTER_CREATION);
+        }
+
+        public bool PersistInHistory()
+        {
+            return true;
+        }
     }
 }
