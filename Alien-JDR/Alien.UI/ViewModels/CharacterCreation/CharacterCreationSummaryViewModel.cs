276298fa64de14b63ea631bc09dda0a53a947d8b
Soultan Hatsijev <soultan_98@hotmail.com>
2021-12-03 15:28:16 +0100

    feat/refactor: Configured ViewModel with basic functions. Rewrote and redesigned Summary view

--- Alien-JDR/Alien.UI/ViewModels/CharacterCreation/CharacterCreationSummaryViewModel.cs
+++ Alien-JDR/Alien.UI/ViewModels/CharacterCreation/CharacterCreationSummaryViewModel.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Alien.BLL.Dtos;
+using Alien.BLL.Interfaces;
+using Alien.UI.Helpers;
+using Alien.UI.States;
+using Prism.Regions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,49 @@ using System.Threading.Tasks;
 
 namespace Alien.UI.ViewModels
 {
-    class CharacterCreationSummaryViewModel
+    public class CharacterCreationSummaryViewModel : ViewModelBase, IJournalAware
     {
+        private readonly ICharacterService _characterService;
+
+        private CharacterCreationDto _characterCreation;
+
+        public CharacterCreationDto CharacterCreation
+        {
+            get { return _characterCreation; }
+            set { SetProperty(ref _characterCreation, value); }
+        }
+
+
+        public CharacterCreationSummaryViewModel(IRegionNavigationService regionNavigationService, IAuthenticator authenticator, ICharacterService characterService)
+            : base(regionNavigationService, authenticator)
+        {
+            _characterService = characterService ??
+                throw new ArgumentNullException(nameof(characterService));
+        }
+
+        public void NavigateBack()
+        {
+            if (_regionNavigationService.Journal.CanGoBack)
+            {
+                _regionNavigationService.Journal.GoBack();
+            }
+        }
+
+        public void CreateCharacter()
+        {
+
+        }
+
+        public override void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            base.OnNavigatedTo(navigationContext);
+
+            CharacterCreation = navigationContext.Parameters.GetValue<CharacterCreationDto>(Global.CHARACTER_CREATION);
+        }
+
+        public bool PersistInHistory()
+        {
+            return true;
+        }
     }
 }
