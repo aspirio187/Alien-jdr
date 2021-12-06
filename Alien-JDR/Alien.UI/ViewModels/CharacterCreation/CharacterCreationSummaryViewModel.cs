65d3772203d1d44678bde9cca296d08afd03108a
Soultan Hatsijev <soultan_98@hotmail.com>
2021-12-06 14:30:01 +0100

    feat: Created character creation public infos view

--- Alien-JDR/Alien.UI/ViewModels/CharacterCreation/CharacterCreationSummaryViewModel.cs
+++ Alien-JDR/Alien.UI/ViewModels/CharacterCreation/CharacterCreationSummaryViewModel.cs
@@ -2,9 +2,11 @@
 using Alien.BLL.Interfaces;
 using Alien.UI.Helpers;
 using Alien.UI.States;
+using Prism.Commands;
 using Prism.Regions;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,6 +33,14 @@ namespace Alien.UI.ViewModels
             set { SetProperty(ref _careerDescription, value); }
         }
 
+        private DelegateCommand _navigateBackCommand;
+
+        public DelegateCommand NavigateBackCommand => _navigateBackCommand ??= new(NavigateBack);
+
+        private DelegateCommand _createCharacterCommand;
+
+        public DelegateCommand CreateCharacterCommand => _createCharacterCommand ??= new(CreateCharacter);
+
         public CharacterCreationSummaryViewModel(IRegionNavigationService regionNavigationService, IAuthenticator authenticator, ICharacterService characterService)
             : base(regionNavigationService, authenticator)
         {
@@ -46,9 +56,20 @@ namespace Alien.UI.ViewModels
             }
         }
 
-        public void CreateCharacter()
+        public async void CreateCharacter()
         {
-            _characterService.CreateCharacter(CharacterCreation, _authenticator.User.Id);
+            try
+            {
+                if (await _characterService.CreateCharacter(CharacterCreation, _authenticator.User.Id))
+                {
+                    _regionNavigationService.Journal.Clear();
+                    Navigate(ViewsEnum.CharactersView);
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+            }
         }
 
         public override void OnNavigatedTo(NavigationContext navigationContext)
