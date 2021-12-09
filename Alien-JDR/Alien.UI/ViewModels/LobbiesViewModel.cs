874df3e521f9ff15d9a044e64cfbe871f18143da
Soultan Hatsijev <soultan_98@hotmail.com>
2021-12-09 15:32:08 +0100

    feat/refactor: Added first commands and list of lobbies in ViewModel. Redesigned the view

--- Alien-JDR/Alien.UI/ViewModels/LobbiesViewModel.cs
+++ Alien-JDR/Alien.UI/ViewModels/LobbiesViewModel.cs
@@ -1,8 +1,11 @@
-﻿using Alien.UI.States;
+﻿using Alien.BLL.Dtos;
+using Alien.BLL.Interfaces;
+using Alien.UI.States;
 using Prism.Commands;
 using Prism.Regions;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,17 +14,22 @@ namespace Alien.UI.ViewModels
 {
     public class LobbiesViewModel : ViewModelBase
     {
+        private readonly ILobbyService _lobbyService;
+
+        public ObservableCollection<LobbyDto> Lobbies { get; set; }
+
         public override DelegateCommand LoadCommand => _loadCommand ??= new DelegateCommand(async () => await LoadAsync());
 
-        public LobbiesViewModel(IRegionNavigationService regionNavigationService, IAuthenticator authenticator)
+        public LobbiesViewModel(IRegionNavigationService regionNavigationService, IAuthenticator authenticator, ILobbyService lobbyService)
             : base(regionNavigationService, authenticator)
         {
-
+            _lobbyService = lobbyService ??
+                throw new ArgumentNullException(nameof(lobbyService));
         }
 
         protected override async Task LoadAsync()
         {
-            // TODO : Charger toutes les parties
+            Lobbies = new ObservableCollection<LobbyDto>(await _lobbyService.GetLobbiesAsync());
         }
     }
 }
