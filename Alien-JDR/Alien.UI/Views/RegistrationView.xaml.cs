571ab7c02ac4962f05994c7dc38e179b8beba4ca
Soultan Hatsijev <soultan_98@hotmail.com>
2021-12-18 12:09:36 +0100

    fix: Commented RegistrationView Register btn logic

--- Alien-JDR/Alien.UI/Views/RegistrationView.xaml.cs
+++ Alien-JDR/Alien.UI/Views/RegistrationView.xaml.cs
@@ -44,54 +44,54 @@ namespace Alien.UI.Views
 
         private void Registration_Click(object sender, RoutedEventArgs e)
         {
-            SmtpClient Client = new SmtpClient()
-            {
-                Host = "smtp.gmail.com",
-                Port = 587,
-                EnableSsl = true,
-                DeliveryMethod = SmtpDeliveryMethod.Network,
-                UseDefaultCredentials = false,
-                Credentials = new NetworkCredential()
-                {
-                    UserName = "alienjdrpac@gmail.com",
-                    Password = "qyizyoiddmvoaywp"
-                }
-            };
+            //SmtpClient Client = new SmtpClient()
+            //{
+            //    Host = "smtp.gmail.com",
+            //    Port = 587,
+            //    EnableSsl = true,
+            //    DeliveryMethod = SmtpDeliveryMethod.Network,
+            //    UseDefaultCredentials = false,
+            //    Credentials = new NetworkCredential()
+            //    {
+            //        UserName = "alienjdrpac@gmail.com",
+            //        Password = "qyizyoiddmvoaywp"
+            //    }
+            //};
 
-            MailAddress FromEmail = new MailAddress("alienjdrpac@gmail.com", "Alien JDR");
-            try
-            {
-                MailAddress ToEmail = new MailAddress(txbEmail.Text, "Utilisateur");
-                MailMessage Message = new MailMessage()
-                {
-                    From = FromEmail,
-                    Subject = "Confiramation d'enregistrement",
-                    Body = "\t\tALIEN, LE JEU DE ROLE, PLONGEZ DANS L'UNIVERS DE SCIENCE-FICTION HORRIFIQUE DE RIDLEY SCOTTE! " +
-                           "\n\n \tL'espace est vaste, sombre, et n'est pas un environnement amical. Les rayons gamma et les neutrinos jaillissent des " +
-                           "\n \tétoiles mourantes pour vous brûler vif, les trous noirs vous déchirent, et le vide lui-même fait bouillir votre sang et gèle " +
-                           "\n \tvotre cerveau. Essayez de crier et personne ne peut vous entendre - retenez votre respiration et vous vous brisez les" +
-                           "\n \tpoumons." +
-                           "\n \tMais l'espace n'est pas aussi vide qu'on pourrait le croire. Ses frontières ne cessent de s'élargir. Les gouvernements" +
-                           "\n \trivaux mènent une guerre froide tandis que les corporations se disputent des ressources précieuses. Les colons se" +
-                           "\n \ttournent vers les étoiles et jouent avec leur vie : chaque nouveau monde apprivoisé offre soit un festin, soit la famine. Et" +
-                           "\n \til y a des choses qui se cachent dans l'ombre de chaque astéroïde, des choses étranges, différentes et mortelles." +
-                           "\n \tDes choses Aliens...",
+            //MailAddress FromEmail = new MailAddress("alienjdrpac@gmail.com", "Alien JDR");
+            //try
+            //{
+            //    MailAddress ToEmail = new MailAddress(txbEmail.Text, "Utilisateur");
+            //    MailMessage Message = new MailMessage()
+            //    {
+            //        From = FromEmail,
+            //        Subject = "Confiramation d'enregistrement",
+            //        Body = "\t\tALIEN, LE JEU DE ROLE, PLONGEZ DANS L'UNIVERS DE SCIENCE-FICTION HORRIFIQUE DE RIDLEY SCOTTE! " +
+            //               "\n\n \tL'espace est vaste, sombre, et n'est pas un environnement amical. Les rayons gamma et les neutrinos jaillissent des " +
+            //               "\n \tétoiles mourantes pour vous brûler vif, les trous noirs vous déchirent, et le vide lui-même fait bouillir votre sang et gèle " +
+            //               "\n \tvotre cerveau. Essayez de crier et personne ne peut vous entendre - retenez votre respiration et vous vous brisez les" +
+            //               "\n \tpoumons." +
+            //               "\n \tMais l'espace n'est pas aussi vide qu'on pourrait le croire. Ses frontières ne cessent de s'élargir. Les gouvernements" +
+            //               "\n \trivaux mènent une guerre froide tandis que les corporations se disputent des ressources précieuses. Les colons se" +
+            //               "\n \ttournent vers les étoiles et jouent avec leur vie : chaque nouveau monde apprivoisé offre soit un festin, soit la famine. Et" +
+            //               "\n \til y a des choses qui se cachent dans l'ombre de chaque astéroïde, des choses étranges, différentes et mortelles." +
+            //               "\n \tDes choses Aliens...",
 
                             
-                           IsBodyHtml = true,
+            //               IsBodyHtml = true,
 
-                };
+            //    };
 
 
-                Message.To.Add(ToEmail);
+            //    Message.To.Add(ToEmail);
 
-                Client.SendCompleted += Client_SendCompleted;
-                Client.SendMailAsync(Message);
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex.Message);
-            }
+            //    Client.SendCompleted += Client_SendCompleted;
+            //    Client.SendMailAsync(Message);
+            //}
+            //catch (Exception ex)
+            //{
+            //    Debug.WriteLine(ex.Message);
+            //}
         }
 
         private void Client_SendCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
